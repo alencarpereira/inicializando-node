e828799749c46d442c81c658dadfdb8426a35892
alencarpereira <suporte@alencarpereira.com>
2025-05-03 01:20:13 -0300

    commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+
+app.get('/usuario', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+
+})
+
+app.post('/usuario', async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+
+    res.status(201).json(user)
+})
+
+app.listen(3000)
